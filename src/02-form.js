99b9f25726f78d5de2c706581d1bc47e2222db1f
KrystianRyczek <153994194+KrysianRyczek@users.noreply.github.com>
2024-07-06 16:57:28 +0200

    task form done
    task form done

---
+++ src/02-form.js
@@ -0,0 +1,31 @@
+    
+   const localMemory={
+     "#email": localStorage.getItem(document.querySelector("input").id),
+     "#msg": localStorage.getItem(document.querySelector("textarea").id)
+   }
+   console.log(localMemory)
+   for (const record in localMemory) {
+    if (localMemory[record]===null) {
+        continue
+    }
+    console.log(localMemory[record])
+    const element = document.querySelector(record)
+
+    element.value=localMemory[record]
+    console.log(element)
+  }
+
+    const form =document.querySelector(".feedback-form")
+    console.log(form)
+    
+    form.addEventListener("input", (event) => {
+        event.preventDefault()
+        const currentObject = event.target
+        console.log(currentObject.value)
+        localStorage.setItem(currentObject.id, currentObject.value);
+    })
+
+    form.addEventListener("submit", (event) => {
+        localStorage.removeItem(document.querySelector("input").id);
+        localStorage.removeItem(document.querySelector("textarea").id);
+    })
